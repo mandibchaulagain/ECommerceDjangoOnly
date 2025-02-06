56751b214b4664fe6c3bcfaac078414761aca94e
mandibchaulagain <mandibchaulagain8@gmail.com>
2025-02-06 15:19:54 +0545

    added some data

--- clothing_ecom/homepage/models.py
+++ clothing_ecom/homepage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+    category = models.CharField(max_length=100, choices = [
+        ('Men','Men'),
+        ('Women', 'Women'),
+        ('Accessories', 'Accessories'),
+    ])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- clothing_ecom/homepage/views.py
+++ clothing_ecom/homepage/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 def home(request):
-    return render(request, 'homepage/index.html')
+    new_arrivals = Product.objects.order_by('-created_at')[:6]
+    sale_products = Product.objects.order_by('-created_at')[:6]
+
+    return render(request, 'homepage/index.html',{
+        'new_arrivals':new_arrivals,
+        'sale_products':sale_products
+    })
